fcdd338da1bb4dbad178e5212187238f286d2a4c
Sebastien Binet <binet@cern.ch>
2014-01-03 17:16:37 +0100

    all: test non-system-wide include dirs

--- gccxml_test.go
+++ gccxml_test.go
@@ -29,6 +29,31 @@ func TestConstants(t *testing.T) {
 	}
 }
 
+func TestIncludes(t *testing.T) {
+	g := Xml{
+		File: "testdata/include/test_includes.h",
+		CFlags: nil,
+		IncludeDirs: []string{"testdata"},
+	}
+	ms, _ := g.Macros()
+	ms = ms.Constants("GOGCCXML_")
+	table := map[string]string{
+		"GOGCCXML_MAJOR_VERSION": "0",
+		"GOGCCXML_MINOR_VERSION": "0",
+		"GOGCCXML_PATCHLEVEL":   "1",
+		"GOGCCXML_42":  "(42)",
+	}
+	if len(table) != len(ms) {
+		t.Fatalf("expected [%d] items. got [%d]\n", len(table), len(ms))
+	}
+
+	for _, m := range ms {
+		if table[m.Name] != m.Body {
+			t.Fatalf("expected %q for item %q. got %q\n", table[m.Name], m.Name, m.Body)
+		}
+	}
+}
+
 func checkError(err error) {
 	if err != nil {
 		panic(err)
